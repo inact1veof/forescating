b4bfe1559a19a1433a2b602b0f59a34a487550b6
inact1veof <inact1veof_iachernitsin@users.noreply.github.com>
2020-05-07 13:04:53 +0500

    forescating
    Deleted influx and grafana services

--- grafana/public/app/plugins/datasource/opentsdb/types.ts
+++
@@ -1,9 +0,0 @@
-import { DataQuery, DataSourceJsonData } from '@grafana/data';
-
-export interface OpenTsdbQuery extends DataQuery {}
-
-export interface OpenTsdbOptions extends DataSourceJsonData {
-  tsdbVersion: number;
-  tsdbResolution: number;
-  lookupLimit: number;
-}
--- grafana/public/app/plugins/panel/news/module.tsx
+++
@@ -1,6 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-import { NewsPanel } from './NewsPanel';
-import { NewsPanelEditor } from './NewsPanelEditor';
-import { defaults, NewsOptions } from './types';
-
-export const plugin = new PanelPlugin<NewsOptions>(NewsPanel).setDefaults(defaults).setEditor(NewsPanelEditor);
